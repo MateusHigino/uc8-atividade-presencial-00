86d4a3017b4aa0bac70916143ef04d5f27703ac1
Mateus Higino <89665328+MateusHigino@users.noreply.github.com>
2021-09-13 23:21:41 -0300

    Aula de Segunda-Feira

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-let listaPecas=[]
-let nomePeca='Pe'
-let pesoPeca=300
-let podeCadastrar=true
+var listaPecas=[]
+var nomePeca='Peça Teste'
+var pesoPeca=300
+var podeCadastrar=true
 
 if (pesoPeca<100){
     console.log('Não há possibilidade de cadastrar!')
     podeCadastrar=false
 }
 
-if (listaPecas.length > 10){
-    console.log("limite de peças atingido!")
+if (listaPecas.length>10){
+    console.log('Limite de peças atingido!')
     podeCadastrar=false
 }
 
 if (nomePeca.length<3){
-    console.log('Quantidade de letras inferior ao permitido!')
+    console.log('Tamanho de letras inferior ao permitido!')
     podeCadastrar=false
 }
 
 if (podeCadastrar){
     listaPecas.push(nomePeca)
-    console.log("Peça cadastrada com sucesso!")
+    console.log('Cadastramento efetuado com sucesso')
 } else {
-  console.log("Ocorreu um problema para cadastrar a peça!")
-}   
+  console.log('Houve problemas para cadastrar a peça')
+}
 
-console.log('listando as peças!')
-
-for(let i=0;i<listaPecas.length;i++){
+console.log('Listando as peças cadastradas.')
+for (let i=0;i<listaPecas.length;i++){
     console.log(listaPecas[i])
 }
